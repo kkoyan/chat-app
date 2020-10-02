212b5bd2b184e913443950af9e832c0a4bff5f73
kkoyan <71924575+kkoyan@users.noreply.github.com>
2020-10-02 20:15:52 +0900

    deviseに関するビューファイルの配置

--- db/migrate/20201002101607_devise_create_users.rb
+++ db/migrate/20201002101607_devise_create_users.rb
@@ -9,7 +9,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string   :reset_password_token  
       t.datetime :reset_password_sent_at
 
       ## Rememberable
