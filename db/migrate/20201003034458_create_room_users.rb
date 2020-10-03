e092cc99690010da17a24c0bd4701ca003d04a68
kkoyan <71924575+kkoyan@users.noreply.github.com>
2020-10-03 17:06:14 +0900

    プルダウンの実装
    This reverts commit 8657c84b5d2040342b788ebf7de3faf7ef467e03.

--- db/migrate/20201003034458_create_room_users.rb
+++ db/migrate/20201003034458_create_room_users.rb
@@ -2,11 +2,9 @@ class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :room_users do |t|
         ## Database authenticatable
-        t.string :name,               null: false
-        t.string :email,              null: false, default: ""
-        t.string :encrypted_password, null: false, default: ""  
-
-      t.timestamps
+        t.references :room, foreign_key: true
+        t.references :user, foreign_key: true
+        t.timestamps
     end
   end
 end
